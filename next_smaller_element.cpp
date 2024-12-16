94fd6c5777bcb49047807c05d2c30e77fa3c82e8
Ashutosh Roy <rashu2879@gmail.com>
2024-12-16 21:44:42 +0530

    next_smaller_element

--- next_smaller_element.cpp
+++ next_smaller_element.cpp
@@ -3,26 +3,55 @@ using namespace std;
 
 int main(){
 
-    vector<int> nums = {2,1,4,3};
+    // brute approach
+    // T.C = O(N^2)
 
-    vector<int> output;
+    // vector<int> nums = {2,1,4,3};
 
-    for(int i=0;i<nums.size();i++){
+    // vector<int> output;
 
-        int ele = -1;
+    // for(int i=0;i<nums.size();i++){
 
-        for(int j=i+1;j<nums.size();j++){
+    //     int ele = -1;
 
-            if(nums[i] > nums[j]){
-                ele = nums[j];
-                break;
-            }
-        }
-        output.push_back(ele);
-    }
+    //     for(int j=i+1;j<nums.size();j++){
+
+    //         if(nums[i] > nums[j]){
+    //             ele = nums[j];
+    //             break;
+    //         }
+    //     }
+    //     output.push_back(ele);
+    // }
    
+
+    // for(int i=0;i<output.size();i++){
+    //     cout<<output[i]<<" ";
+    // }
+
+    // ---------------------------------------------------------------------->
+
+    // optimized approach
+    // T.C -> O(N)
+
+    vector<int> nums = {2,1,4,3};
+    stack<int> st;
+    vector<int> output(nums.size(), -1);
+
+    for(int i=nums.size()-1;i>=0;i--){
+
+        while(!st.empty() && st.top() >= nums[i]){
+            st.pop();
+        }
+
+        if(!st.empty() && st.top() < nums[i]){
+            output[i] = st.top();
+        }
+        st.push(nums[i]);
+    }
 
     for(int i=0;i<output.size();i++){
         cout<<output[i]<<" ";
     }
+
 }
